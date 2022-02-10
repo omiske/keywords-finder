0c51772b3bebf12c08283fb2a7ee5fdb44d1ac73
Tomeulnv <78488301+Tomeulnv@users.noreply.github.com>
2022-02-10 18:56:47 +0100

    fixed output issue

--- code/keyword-finder.R
+++ code/keyword-finder.R
@@ -3,21 +3,21 @@ files <- str_remove(list.files("../files/txt-files/", full.names = T), "../files
 keywords <- c(readLines("../keywords.txt"))
 
 # Prepare table for search results
-output <- data.frame(matrix(0, nrow = length(files), ncol = length(keywords)+1))
+output <- data.frame(matrix(0, ncol = length(keywords)+1))
 colnames(output) <- c("file", keywords)
-output$file <- str_remove(files, ".pdf.txt")
 
 # Search for matches and return 1 if found
-for (i in length(output$file)){
-  for (fn in files){
-    for (kw in keywords) {
-      if (length(grep(kw, readLines(paste("../files/txt-files/",fn, sep = "")), ignore.case = T)) > 0){
-        output[i, kw] = 1
-      }
+for (fn in files) {
+  for (kw in keywords) {
+    if (length(grep(kw, readLines(paste("../files/txt-files/",fn, sep = "")), ignore.case = T)) > 0){
+          output[fn, kw] = 1
     }
   }
 }
 
 # Save search results
+output = output[-1,]
+output$file = rownames(output)
+output$file = str_remove(output$file, ".pdf.txt")
 library(writexl)
 write_xlsx(output, "../output/search_results.xls")
